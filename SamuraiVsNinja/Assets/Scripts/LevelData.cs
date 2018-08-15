0567c6ccd3d54c28bd0457839ce09962aa994925
Mathias Mustonen <mathias.mustonen@gmail.com>
2018-08-15 18:47:36 +0300

    Movement proto
    Movement test.

---
+++ SamuraiVsNinja/Assets/Scripts/LevelData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class LevelData
+{
+    public Texture2D LevelMap { get; private set; }
+
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+
+    public LevelData(Texture2D levelMap)
+    {
+        LevelMap = levelMap;
+
+        Width = LevelMap.width;
+        Height = LevelMap.height;
+    }   
+}
