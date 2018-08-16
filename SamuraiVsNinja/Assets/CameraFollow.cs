3410c02394e754cb4610a6547e51c1820c85fc8b
Nikl3 <niko.tuuli@hotmail.com>
2018-08-16 14:19:24 +0300

    camera follow and zoom, kinda working now

--- SamuraiVsNinja/Assets/CameraFollow.cs
+++ SamuraiVsNinja/Assets/CameraFollow.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-    public GameObject player;
+    public Transform player;
+    public Transform player2;
     Vector3 offset;
+    public Vector2 cameraMargins;
+    Camera cam;
 
-	void Start () {
-        offset = transform.position - player.transform.position;
+    void UpdateCam() {
+        var p1 = player.position;
+        var p2 = player2.position;
+        var boxMin = Vector2.Min(p1, p2);
+        var boxMax = Vector2.Max(p1, p2);
+        boxMin -= cameraMargins;
+        boxMax += cameraMargins;
+        var boxDim = boxMax - boxMin;
+
+        // clamp..?
+        boxDim.y = Mathf.Max(boxDim.y, 9f); // min cam height
+        cam.orthographicSize = boxDim.y;
+        transform.position = (p1 + p2) /2f - Vector3.forward;    
+
+    }
+    void Start () {
+        //offset = transform.position - player.position;
+        cam = FindObjectOfType<Camera>();
 	}
 	
 	void LateUpdate () {
-        transform.position = player.transform.position + offset;
+        UpdateCam();
 	}
 }
