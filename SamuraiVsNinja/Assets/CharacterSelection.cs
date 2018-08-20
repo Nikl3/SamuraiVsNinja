c60e825c18364daefa709e785bfead053a56fb04
Nikl3 <niko.tuuli@hotmail.com>
2018-08-20 11:41:36 +0300

    CharacterSelection started (based on tutorial from youtube)

---
+++ SamuraiVsNinja/Assets/CharacterSelection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour {
+
+    List<GameObject> models;
+    int selectionIndex = 0;
+
+	void Start () {
+        models = new List<GameObject>();
+        foreach (Transform t in transform) {
+            models.Add(t.gameObject);
+            t.gameObject.SetActive(false);
+            
+        }
+        models[selectionIndex].SetActive(true);
+    }
+
+    public void Select(int index) {
+        if (index == selectionIndex) {
+            return;
+        }
+        if (index < 0 || index >= models.Count) {
+            return;
+        }
+        models[selectionIndex].SetActive(false);
+        selectionIndex = index;
+        models[selectionIndex].SetActive(true);
+    }
+    
+}
