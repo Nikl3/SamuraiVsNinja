392ec439769e4b65e6b83c10770265ef2bc2c1ba
Mathias Mustonen <mathias.mustonen@gmail.com>
2018-08-22 00:29:41 +0300

    Random fade
    Fade system created and added inside SceneManager, for now!

--- SamuraiVsNinja/Assets/Scripts/UI/SceneMaster.cs
+++ SamuraiVsNinja/Assets/Scripts/UI/SceneMaster.cs
@@ -1,7 +1,71 @@
-﻿using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneMaster : SingeltonPersistant<SceneMaster>
 {
+    private float delayTime = 1f;
+    private bool isFading = false;
+    private Image screenFadeImage;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        screenFadeImage = GameObject.Find("FadeImage").GetComponent<Image>();
+        screenFadeImage.fillAmount = 1f;
+    }
+
+    private void Start()
+    {
+        Invoke("DelayStart", delayTime);
+    }
+
+    private void DelayStart()
+    {
+        RandomizeFillMethod();
+        FadeScreenImage(0);
+    }
+
+    private void RandomizeFillMethod()
+    {
+        int randomIndex = RandomizeNumbers(0, 4);
+        switch (randomIndex)
+        {
+            case 0:
+                screenFadeImage.fillMethod = Image.FillMethod.Horizontal;
+                screenFadeImage.fillOrigin = RandomizeNumbers(0, 1);
+                break;
+            case 1:
+                screenFadeImage.fillMethod = Image.FillMethod.Vertical;
+                screenFadeImage.fillOrigin = RandomizeNumbers(0, 1);
+                break;
+            case 2:
+                screenFadeImage.fillMethod = Image.FillMethod.Radial90;
+                screenFadeImage.fillClockwise = RandomizeNumbers(0, 1) == 1 ? true : false;
+                screenFadeImage.fillOrigin = RandomizeNumbers(0, 3);
+                break;
+            case 3:
+                screenFadeImage.fillMethod = Image.FillMethod.Radial180;
+                screenFadeImage.fillClockwise = RandomizeNumbers(0, 1) == 1 ? true : false;
+                screenFadeImage.fillOrigin = RandomizeNumbers(0, 3);
+                break;
+            case 4:
+                screenFadeImage.fillMethod = Image.FillMethod.Radial360;
+                screenFadeImage.fillClockwise = RandomizeNumbers(0, 1) == 1 ? true : false;
+                screenFadeImage.fillOrigin = RandomizeNumbers(0, 3);
+                break;
+            default:
+                
+                break;
+        }
+    }
+
+    private int RandomizeNumbers(int minValue, int maxValue)
+    {
+        return Random.Range(minValue, maxValue);
+    }
+
     public void LoadScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
@@ -10,5 +74,27 @@ public class SceneMaster : SingeltonPersistant<SceneMaster>
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void FadeScreenImage(float targetFillAmount, float fadeSpeed = 1f)
+    {
+        StartCoroutine(IFadeScreenImage(targetFillAmount, 1f));
+    }
+
+    private IEnumerator IFadeScreenImage(float targetFillAmount, float fadeSpeed)
+    {
+        isFading = true;
+        screenFadeImage.raycastTarget = true;
+
+        while (screenFadeImage.fillAmount != targetFillAmount)
+        {
+            screenFadeImage.fillAmount += screenFadeImage.fillAmount < targetFillAmount ? (1f / fadeSpeed) * Time.unscaledDeltaTime : -(1f / fadeSpeed) * Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        screenFadeImage.raycastTarget = false;
+        isFading = false;
+
+        //yield return new WaitForSecondsRealtime(1);
     }
 }
