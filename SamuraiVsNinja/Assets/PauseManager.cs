71bdcaf2253bcbb09acb0b807b6e078d9fce6acb
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2018-09-04 14:46:10 +0300

    Game Pause started.

---
+++ SamuraiVsNinja/Assets/PauseManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour {
+    public Canvas PauseCanvas;
+
+
+    private void Awake()
+    {
+        PauseCanvas.GetComponent<Canvas>();
+
+    }
+
+    private void Start()
+    {
+        PauseCanvas.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        GamePaused();
+
+    }
+
+    private void GamePaused()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseCanvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        //if (Input.GetButtonDown())
+
+    }
+
+    public void ContinueButton()
+    {
+        PauseCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+
+}
+
+
