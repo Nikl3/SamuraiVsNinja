e1e911f90b06353b9aac8d27c5b70ff425a7d7cd
Nikl3 <niko.tuuli@hotmail.com>
2018-10-09 16:54:29 +0300

    Intro video done and working switching scenes automaticly

---
+++ SamuraiVsNinja/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+    void SceneSwitch() {
+        SceneManager.LoadScene(1);
+    }
+
+	void Start () {
+        Invoke("SceneSwitch", 37f);
+	}
+	
+}
